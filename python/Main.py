f5c65ebd85fab8efdd1764e2ba8b17a4a0c5f8af
Andaru <andarudanurdara@yahoo.com>
2021-11-17 23:27:04 +0700

    Tambah Python

---
+++ python/Main.py
@@ -0,0 +1,81 @@
+# /*
+#   Nama File : Main.py
+#   Nama      : Andaru Danurdara Wibisana, Fauzan Azmi Dwicahyo, Irfan Kamal, Muhammad Attila An Naufal
+#   NPM       : 140810200020, 140810200030, 140810200032, 140810200048
+#   Kelas     : B
+#   Tanggal   : 17 November 2021
+#   Deskripsi : Implementasi kasus linked list nilai sidang menggunakan C++, class Person dan Student
+# *********************************************/
+
+from Student import Student
+from StudentList import StudentList
+
+def addDummyData(list):
+    list.insertLast(Student("Fauzan", 19, "0001", 80, 82, 90))
+    list.insertLast(Student("Attila", 29, "0002", 100, 100, 100))
+    list.insertLast(Student("Andaru", 20, "0003", 85, 87, 92))
+    list.insertLast(Student("Irfan", 19, "0004", 60, 80, 100))
+    return list
+
+def filterMenu(list):
+    print("1. Lebih besar dari")
+    print("2. Lebih kecil dari")
+    sel = input("Pilihan: ")
+    nilai = input("Nilai: ")
+    nilai = int(nilai)
+
+    list.printFilteredTable(sel, nilai)
+
+def addMahasiswaMenu(list):
+    name = input("Nama mahasiswa: ")
+    age = input("Umur mahasiswa: ")
+    npm = input("NPM mahasiswa: ")
+    nilaiPenguji1 = input("Nilai penguji 1: ")
+    nilaiPenguji2 = input("Nilai penguji 2: ")
+    nilaiPembimbing = input("Nilai pembimbing: ")
+    age = int(age)
+    nilaiPenguji1 = float(nilaiPenguji1)
+    nilaiPenguji2 = float(nilaiPenguji2)
+    nilaiPembimbing = float(nilaiPembimbing)
+    if list.insertLast(Student(name, age, npm, nilaiPenguji1, nilaiPenguji2, nilaiPembimbing)) is False:
+        print("NPM sudah terdaftar!\n")
+    else:
+        list.insertLast(Student(name, age, npm, nilaiPenguji1, nilaiPenguji2, nilaiPembimbing))
+    return list
+
+def removeMahasiswaMenu(list):
+    npm = input("NPM mahasiswa yang ingin dihapus: ")
+    temp = list.deleteByNpm(npm)
+    if temp is None :
+        print("NPM tidak terdaftar!")
+    else:
+        del(temp)
+    return list
+
+if __name__ == "__main__":
+    list = StudentList()
+    confirm = True
+
+    while confirm == True:
+        print("1. Lhat tabel mahasiswa")
+        print("2. Lihat tabel terfilter mahasiswa")
+        print("3. Tambah mahasiswa")
+        print("4. Hapus mahasiswa")
+        if list.empty() == True:
+            print("5. Tambah data dummy")
+        print("0. Keluar\n")
+        sel = input('Pilihan: ')
+
+        if sel == '1':
+            list.printTable()
+        elif sel == '2':
+            filterMenu(list)
+        elif sel == '3':
+            list = addMahasiswaMenu(list)
+        elif sel == '4':
+            list = removeMahasiswaMenu(list)
+        elif sel == '5':
+            if list.empty() == True:
+                list = addDummyData(list)
+        elif sel == '0':
+            confirm = False
---
+++ python/Student.py
@@ -0,0 +1,92 @@
+# /*
+#   Nama File : Student.py
+#   Nama      : Andaru Danurdara Wibisana, Fauzan Azmi Dwicahyo, Irfan Kamal, Muhammad Attila An Naufal
+#   NPM       : 140810200020, 140810200030, 140810200032, 140810200048
+#   Kelas     : B
+#   Tanggal   : 17 November 2021
+#   Deskripsi : Implementasi kasus nilai sidang dengan singly linked list menggunakan Python, class Student
+# *********************************************/
+
+class Person:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+    
+    def getName(self):
+        return self.__name
+
+    def getAge(self):
+        return self.__age
+    
+    def setName(self, name):
+        self.__name = name
+    
+    def setAge(self, age):
+        self.__age = age
+
+    def __str__(self) -> str:
+        return " Nama : " + self.__name + "\n Umur: " + str(self.__age)
+
+class Student(Person):
+    def __init__(self, name, age, npm, nilaiPenguji1, nilaiPenguji2, nilaiPembimbing):
+        super().__init__(name, age)
+        self.__npm = npm
+        self.__nilaiPenguji1 = nilaiPenguji1
+        self.__nilaiPenguji2 = nilaiPenguji2
+        self.__nilaiPembimbing = nilaiPembimbing
+        self.next = None
+
+    def getNpm(self):
+        return self.__npm
+    
+    def setNpm(self, npm):
+        self.__npm = npm
+    
+    def getNilaiPenguji1(self):
+        return self.__nilaiPenguji1
+    
+    def setNilaiPenguji1(self, nilaiPenguji1):
+        self.__nilaiPenguji1 = nilaiPenguji1
+
+    def getNilaiPenguji2(self):
+        return self.__nilaiPenguji2
+    
+    def setNilaiPenguji2(self, nilaiPenguji2):
+        self.__nilaiPenguji2 = nilaiPenguji2
+    
+    def getNilaiPembimbing(self):
+        return self.__nilaiPembimbing
+    
+    def setNilaiPembimbing(self, nilaiPembimbing):
+        self.__nilaiPembimbing = nilaiPembimbing
+
+    def getNilaiAkhir(self):
+        return self.__nilaiPenguji1*float(0.3) + self.__nilaiPenguji2*float(0.3) + self.__nilaiPembimbing*float(0.4)
+    
+    def getHurufMutu(self):
+        nilaiAkhir = self.getNilaiAkhir()
+
+        if nilaiAkhir > 0 and nilaiAkhir <= 60:
+            return 'T'
+        elif nilaiAkhir > 60 and nilaiAkhir <= 70:
+            return 'C'
+        elif nilaiAkhir > 70 and nilaiAkhir <= 80:
+            return 'B'
+        elif nilaiAkhir > 80 and nilaiAkhir <= 100:
+            return 'A'
+        
+        return 'T'
+    
+    def getStatus(self):
+        hurufMutu = self.getHurufMutu()
+
+        if hurufMutu == 'T':
+            return "Tidak Lulus !!"
+
+        return "Lulus !!"
+
+    def __str__(self) -> str:
+        return super().__str__() + "\n NPM : " + self.__npm + "\n Nilai Penguji 1 : " + str(self.__nilaiPenguji1) + "\n Nilai Penguji 2 : " + str(self.__nilaiPenguji2) + "\n Nilai Pembimbing : " + str(self.__nilaiPembimbing) + "\n Nilai Akhir : " + str(self.getNilaiAkhir()) + "\n Huruf Mutu : " + self.getHurufMutu() + "\n Status : " + self.getStatus()
+
+    def output(self):
+        print(self.__str__())
